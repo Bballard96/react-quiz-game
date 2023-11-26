f5a06fee1de0f570d8e5829ac20bd3e7c365ea55
Brendan96 <Brendan.ballard96@yahoo.com>
2023-11-26 11:17:59 -0600

    comment out existing default text

--- src/App.js
+++ src/App.js
@@ -67,16 +67,16 @@ function App() {
       <header className="App-header">
         <p>
 
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
